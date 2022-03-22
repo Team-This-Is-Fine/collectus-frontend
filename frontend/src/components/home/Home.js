f5445c6da83794ce0b232742203cad938500a4b9
mickey <damapongm@gmail.com>
2022-03-22 14:53:13 -0700

    setting up home.js

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,14 +1,28 @@
-import CollectionList from "../collectionList/CollectionList";
+import CollectionList from '../collectionList/CollectionList';
 import { CollectionsContext } from '../../CollectionsContext';
+import axios from 'axios';
+import React, { useState, useEffect, useDebugValue } from 'react';
 
-export default function Home()
-{
-  return (
-    <div className="home-container">
-      <p>Home loaded</p>
-      <CollectionsContext.Provider value={{}}>
-        <CollectionList />
-      </CollectionsContext.Provider>
-    </div >
-  )
+export default function Home() {
+	const [collections, setCollections] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(url)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setCollections([...data]);
+			});
+	}, []);
+
+	return (
+		<div className='home-container'>
+			<p>Home loaded</p>
+			<CollectionsContext.Provider value={{}}>
+				<CollectionList />
+			</CollectionsContext.Provider>
+		</div>
+	);
 }
