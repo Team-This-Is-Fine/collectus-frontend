223ac82829f71db5e9c198b0f330bde719c1807e
JordyAM <JAlexanderMcLean@Gmail.com>
2022-03-22 14:56:22 -0400

    basic structure and index.js setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.set('port', process.env.PORT || 8000);
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+
+app.get('/', (req,res) => {
+    return res.redirect ('api/collections');
+})
+
+const collectionController = require('./controllers/collectionController');
+app.use('api/collections', collectionController);
+
+const itemController = require('./controllers/itemController');
+app.use('api/items', itemController);
