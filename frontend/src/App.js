57e13c7eaf3e491a88a4e32aa59d24bae66f84e8
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-03-24 11:04:34 -0700

    Trying to pull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,16 @@ import CollectionsForm from './components/collectionsForm/CollectionsForm';
 import Items from './components/items/Items';
 import ItemsForm from './components/itemsForm/ItemsForm';
 import { Routes, Route, Link } from 'react-router-dom';
+import { Image } from 'react-bootstrap';
 
 function App()
 {
 	return (
 		<div className="app-container">
 			<header>
-				<h1>Collectus</h1>
-				<nav>
-					<Link to='/collections'>Home</Link>
-				</nav>
+				<Link to='/collections'>
+					<Image src="http://tinyimg.io/i/fAUZtrg.png" alt="Logo" />
+				</Link>
 			</header>
 			<Routes>
 				<Route path='/collections' element={<Collections />} />
--- frontend/src/components/collectionsForm/CollectionsForm.js
+++ frontend/src/components/collectionsForm/CollectionsForm.js
@@ -1,11 +1,21 @@
-import { Modal, ModalForm } from 'react-bootstrap';
+import { Modal, Form, Button } from 'react-bootstrap';
 
 export default function CollectionsForm()
 {
   return (
     <Modal>
       <Modal.Header>Add Collection</Modal.Header>
-      <Modal.Body>Form Properties</Modal.Body>
+      <Modal.Body>
+        <Form.Group>
+          <Form.Label>Name: </Form.Label>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Image Url: </Form.Label>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Name: </Form.Label>
+        </Form.Group>
+      </Modal.Body>
       <Modal.Footer>
         {/* <Button onCLick={handleSubmit}>Submit</Button> */}
       </Modal.Footer>
