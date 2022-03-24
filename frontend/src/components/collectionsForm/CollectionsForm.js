b23695e258c394e59af418d923a3eec81fab4da7
mickey <damapongm@gmail.com>
2022-03-24 10:59:41 -0700

    add form

--- frontend/src/components/collectionsForm/CollectionsForm.js
+++ frontend/src/components/collectionsForm/CollectionsForm.js
@@ -1,14 +1,28 @@
-import { Modal, ModalForm } from 'react-bootstrap';
+import { Modal, Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
 
-export default function CollectionsForm()
-{
-  return (
-    <Modal>
-      <Modal.Header>Add Collection</Modal.Header>
-      <Modal.Body>Form Properties</Modal.Body>
-      <Modal.Footer>
-        {/* <Button onCLick={handleSubmit}>Submit</Button> */}
-      </Modal.Footer>
-    </Modal>
-  )
+export default function CollectionsForm({ showModal, setShowModal }) {
+	const [name, setname] = useState('');
+	const [url, seturl] = useState('');
+
+	function handleChange(e) {
+		setname([e.target.value]);
+	}
+	function handleClose() {
+		setShowModal(!showModal);
+	}
+	return (
+		<Modal show={showModal}>
+			<Modal.Header>Add Collection</Modal.Header>
+			<Modal.Body>
+				<Form.Group>
+					<Form.Label>name: </Form.Label>
+					<Form.Control type='text' onChange={handleChange} value={name} />
+				</Form.Group>
+			</Modal.Body>
+			<Modal.Footer>
+				{<Button onClick={handleClose}>Close</Button>}
+			</Modal.Footer>
+		</Modal>
+	);
 }
