a14ea16b52db17dd71c8e7c00c65f9c617e248aa
oriofir <97814056+oriofir@users.noreply.github.com>
2022-03-24 11:31:39 -0700

    item modal template working

--- frontend/src/components/itemsForm/ItemsForm.js
+++ frontend/src/components/itemsForm/ItemsForm.js
@@ -1,14 +1,12 @@
-import { Modal, ModalForm } from 'react-bootstrap';
-
-export default function ItemsForms()
-{
-  return (
-    <Modal>
-      <Modal.Header>Add Collection</Modal.Header>
-      <Modal.Body>Form Properties</Modal.Body>
-      <Modal.Footer>
-        {/* <Button onCLick={handleSubmit}>Submit</Button> */}
-      </Modal.Footer>
-    </Modal>
-  )
-}
+// export default function ItemsForms()
+// {
+//   return (
+//     <Modal>
+//       <Modal.Header>Add Collection</Modal.Header>
+//       <Modal.Body>Form Properties</Modal.Body>
+//       <Modal.Footer>
+//         {/* <Button onCLick={handleSubmit}>Submit</Button> */}
+//       </Modal.Footer>
+//     </Modal>
+//   )
+// }
