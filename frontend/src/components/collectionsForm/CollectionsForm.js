9eb8d01fde0166063ddb56d89ebb03aabfe2d874
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-03-25 11:21:20 -0700

    Merging

--- frontend/src/components/collectionsForm/CollectionsForm.js
+++ frontend/src/components/collectionsForm/CollectionsForm.js
@@ -4,77 +4,84 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 export default function CollectionsForm({
-	collections,
-	setCollections,
-	setShowModal,
-}) {
-	const empty = {
-		name: '',
-		img: '',
-	};
+  collections,
+  setCollections,
+  setShowModal,
+})
+{
+  const empty = {
+    name: '',
+    img: '',
+  };
 
-	const [newCollection, setNewCollection] = useState(empty);
-	const navigate = useNavigate();
+  const [newCollection, setNewCollection] = useState(empty);
+  const navigate = useNavigate();
 
-	function handleNameChange(e) {
-		setNewCollection({ ...newCollection, name: e.target.value });
-	}
+  function handleNameChange(e)
+  {
+    setNewCollection({ ...newCollection, name: e.target.value });
+  }
 
-	function handleImageChange(e) {
-		setNewCollection({ ...newCollection, img: e.target.value });
-	}
+  function handleImageChange(e)
+  {
+    setNewCollection({ ...newCollection, img: e.target.value });
+  }
 
-	function handleSubmit(e) {
-		axios
-			.post('http://localhost:8000/api/collections/', newCollection)
-			.then((res) => {
-				setCollections([...collections, res]);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
+  function handleSubmit(e)
+  {
+    axios
+      .post('http://localhost:8000/api/collections/', newCollection)
+      .then((res) =>
+      {
+        setCollections([...collections, res]);
+      })
+      .catch((error) =>
+      {
+        console.log(error);
+      });
 
-		setShowModal(false);
-		navigate('/collections');
-	}
+    setShowModal(false);
+    navigate('/collections');
+  }
 
-	function handleClose() {
-		setShowModal(false);
-	}
+  function handleClose()
+  {
+    setShowModal(false);
+  }
 
-	return (
-		<Modal show={true}>
-			<Modal.Header>Add Collection</Modal.Header>
+  return (
+    <Modal show={true}>
+      <Modal.Header>Add Collection</Modal.Header>
 
-			<Modal.Body>
-				<Form onSubmit={handleSubmit}>
-					<Form.Group>
-						<Form.Label>name: </Form.Label>
-						<Form.Control
-							type='text'
-							onChange={handleNameChange}
-							value={newCollection.name}
-							required
-						/>
-					</Form.Group>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>name: </Form.Label>
+            <Form.Control
+              type='text'
+              onChange={handleNameChange}
+              value={newCollection.name}
+              required
+            />
+          </Form.Group>
 
-					<Form.Group>
-						<Form.Label>Image Url: </Form.Label>
-						<Form.Control
-							type='text'
-							onChange={handleImageChange}
-							value={newCollection.img}
-							className='collection-image'
-							required
-						/>
-					</Form.Group>
-					<Button type='submit'>Submit</Button>
-				</Form>
-			</Modal.Body>
+          <Form.Group>
+            <Form.Label>Image Url: </Form.Label>
+            <Form.Control
+              type='text'
+              onChange={handleImageChange}
+              value={newCollection.img}
+              className='collection-image'
+              required
+            />
+          </Form.Group>
+          <Button type='submit'>Submit</Button>
+        </Form>
+      </Modal.Body>
 
-			<Modal.Footer>
-				<Button onClick={handleClose}>Close</Button>
-			</Modal.Footer>
-		</Modal>
-	);
+      <Modal.Footer>
+        <Button onClick={handleClose}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
--- frontend/src/components/collectionsView/CollectionsView.js
+++ frontend/src/components/collectionsView/CollectionsView.js
@@ -1,57 +1,60 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Card, Col, Row, Button } from 'react-bootstrap';
 import './CollectionsView.css';
 import axios from 'axios';
 
 // Handles how collections are rendered.
-export default function CollectionsView({ collections }) {
-	function handleDelete(id) {
-		console.log(id);
+export default function CollectionsView({ collections })
+{
+  function handleDelete(id)
+  {
+    console.log(id);
 
-		axios
-			.delete(`http://localhost:8000/api/collection/${id}`)
-			.then((res) => {});
-	}
+    axios
+      .delete(`http://localhost:8000/api/collection/${id}`)
+      .then((res) => { });
+  }
 
-	return (
-		<div className='card-container'>
-			<Row xs={1} md={2} lg={3} xl={4} className='g-4'>
-				{collections.map((element) => {
-					return (
-						<Col key={element._id}>
-							<Card className='h-100'>
-								{element.img && (
-									<Card.Img
-										variant='top'
-										src={element.img ? element.img : ''}
-										alt={element.name}
-									/>
-								)}
-								<Card.Body>
-									{element.img ? (
-										''
-									) : (
-										<Card.Title>No Image Available</Card.Title>
-									)}
-									<Card.Text className='text-muted'>{element.name}</Card.Text>
-								</Card.Body>
-								<Card.Footer>
-									<Link to={`/collections/${element._id}`}>
-										<Button variant='outline-dark'>View Collection</Button>
-										<Button variant='outline-dark'>Edit Collection</Button>
-										<Button
-											variant='outline-dark'
-											onClick={() => handleDelete(element._id)}>
-											Delete Collection
-										</Button>
-									</Link>
-								</Card.Footer>
-							</Card>
-						</Col>
-					);
-				})}
-				{/* {(activeItem || error) && (
+  return (
+    <div className='card-container'>
+      <Row xs={1} md={2} lg={3} xl={4} className='g-4'>
+        {collections.map((element) =>
+        {
+          return (
+            <Col key={element._id}>
+              <Card className='h-100'>
+                {element.img && (
+                  <Card.Img
+                    variant='top'
+                    src={element.img ? element.img : ''}
+                    alt={element.name}
+                  />
+                )}
+                <Card.Body>
+                  {element.img ? (
+                    ''
+                  ) : (
+                    <Card.Title>No Image Available</Card.Title>
+                  )}
+                  <Card.Text className='text-muted'>{element.name}</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Link to={`/collections/${element._id}`}>
+                    <Button variant='outline-dark'>View Collection</Button>
+                    <Button variant='outline-dark'>Edit Collection</Button>
+                    <Button
+                      variant='outline-dark'
+                      onClick={() => handleDelete(element._id)}>
+                      Delete Collection
+                    </Button>
+                  </Link>
+                </Card.Footer>
+              </Card>
+            </Col>
+          );
+        })}
+        {/* {(activeItem || error) && (
         <Detail
           objectDetail={activeItem}
           show={show}
@@ -59,7 +62,7 @@ export default function CollectionsView({ collections }) {
           error={error}
         />
       )} */}
-			</Row>
-		</div>
-	);
+      </Row>
+    </div>
+  );
 }
